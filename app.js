087b357d34fba860e4db33df3db8b7993a0aec0e
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-09-08 01:25:12 +0300

    edit 8

--- app.js
+++ app.js
@@ -67,14 +67,19 @@ const galleryItems = [
 // Разбей задание на несколько подзадач:
 // Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
 
+const refs = {
+    galleryContainer: document.querySelector('ul.js-gallery'),
+    modal: document.querySelector('.js-lightbox'),
+    modalClose: document.querySelector('[data-action="close-lightbox"]'),
+    imgModal: document.querySelector('img.lightbox__image'),
+    closeBackdrop: document.querySelector('.lightbox__overlay'),
+    galleryArrowLeft: document.querySelector('.lightbox__arrow-left'),
+    galleryArrowRight: document.querySelector('.lightbox__arrow-right'),
 
-
-const galleryContainer = document.querySelector('ul.js-gallery')
-const modal = document.querySelector('.js-lightbox')
-const modalClose = document.querySelector('[data-action="close-lightbox"]')
+}
 
 const galleryMarkup = galleryCollection(galleryItems);
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
+refs.galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 function galleryCollection(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return `
@@ -88,21 +93,51 @@ function galleryCollection(galleryItems) {
     />
   </a>
 </li>`}).join('');
+
+
 }
 // Реализация делегирования на галерее ul.js - gallery и получение url большого изображения.
-galleryContainer.addEventListener('click', onOpenModalClick)
-modalClose.addEventListener('click', closeModalClick)
-modalClose.addEventListener('keyword', closeModalClick)
-galleryContainer.addEventListener('click', onBackdropClick)
+refs.galleryContainer.addEventListener('click', onOpenModalClick)
+refs.modalClose.addEventListener('click', closeModalClick)
+refs.modalClose.addEventListener('keyword', closeModalClick)
+refs.closeBackdrop.addEventListener('click', onBackdropClick)
+refs.galleryArrowLeft.addEventListener('click', onClickArrowLeft)
+refs.galleryArrowRight.addEventListener('click', onClickArrowRight)
 
 // Открытие модального окна по клику на элементе галереи.
 function onOpenModalClick(event) {
     window.addEventListener('keydown', onEscapePress)
     event.preventDefault();
-    modal.classList.add('is-open')
+    refs.modal.classList.add('is-open')
     const target = event.target;
-    console.log(target)
+    onOpenImgModal(event.target.dataset.source, event.target.alt)
+
+
 }
+
+// Подмена значения атрибута src элемента img.lightbox__image.
+function onOpenImgModal(src, alt) {
+    refs.imgModal.src = src;
+    refs.imgModal.alt = alt;
+    flipping()
+}
+
+function flipping(evt) {
+    if (
+        evt.target.classList.contains('lightbox__overlay') ||
+        evt.target.dataset.action === 'close-lightbox'
+    )
+        closeModal();
+    if (
+        evt.target.classList.contains('lightbox__image') ||
+        evt.target.dataset.type === 'next'
+    )
+        onClickArrowRight();
+    if (evt.target.dataset.type === 'prev') onClickArrowLeft();
+
+};
+
+
 
 function onEscapePress(event) {
     const ESC_KEY_CODE = 'Escape';
@@ -110,23 +145,63 @@ function onEscapePress(event) {
         closeModalClick()
     }
 }
-
-
-
-// Подмена значения атрибута src элемента img.lightbox__image.
-
-
 // Закрытие модального окна по клику на кнопку button[data - action= "close-lightbox"].
-
 function closeModalClick(event) {
-    modal.classList.remove('is-open')
+    refs.modal.classList.remove('is-open')
+    onOpenImgModal('', '')
 }
-
 function onBackdropClick(event) {
-    if (event.curentTarget === event.target) {
+    if (event.target === event.currentTarget) {
         closeModalClick()
     }
+    console.log(event)
 }
 // Очистка значения атрибута src элемента img.lightbox__image.Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+//Сделать перелистывание фото
 
+function clickSearchRules(src) {
 
+    const searchRules = galleryItems.indexOf(galleryItems.find(el => el.original === refs.imgModal.src));
+    console.log(searchRules)
+
+}
+
+function onClickArrowRight(src) {
+    let currentImgIndex = clickSearchRules(refs.imgModal.getAttribute('src'));
+    if (currentImgIndex === galleryItems.length - 1) {
+        return currentImgIndex = -1;
+    }
+    onOpenImgModal(
+        galleryItems[currentImgIndex + 1].original,
+        galleryItems[currentImgIndex + 1].description,
+    );
+
+    console.log(currentImgIndex)
+}
+
+function onClickArrowLeft() {
+    let currentImgIndex = clickSearchRules(refs.imgModal.getAttribute('src'));
+    if (currentImgIndex == 0) {
+        onOpenImgModal(
+            galleryItems[currentImgIndex - 1].original,
+            galleryItems[currentImgIndex - 1].description,
+        );
+    } else {
+        currentImgIndex = galleryItems.length;
+    }
+    console.log(currentImgIndex)
+}
+function onKeyDown(e) {
+    if (!modal.classList.contains('is-open')) return;
+    switch (e.code) {
+        case 'Escape':
+            closeModalClick();
+            break;
+        case 'ArrowLeft':
+            onClickArrowLeft();
+            break;
+        case 'ArrowRight':
+            onClickArrowRight();
+            break;
+    }
+}
